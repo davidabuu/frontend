787be171a5dcfcb0661cd1c6b6288c9fb58992ce
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2024-11-08 00:03:14 +0100

    Update PageNextJs.tsx

--- nextjs/PageNextJs.tsx
+++ nextjs/PageNextJs.tsx
@@ -7,7 +7,6 @@ import type { Props as PageProps } from 'nextjs/getServerSideProps';
 import config from 'configs/app';
 import useAdblockDetect from 'lib/hooks/useAdblockDetect';
 import useGetCsrfToken from 'lib/hooks/useGetCsrfToken';
-import * as metadata from 'lib/metadata';
 import * as mixpanel from 'lib/mixpanel';
 import { init as initSentry } from 'lib/sentry/config';
 
@@ -15,14 +14,11 @@ interface Props<Pathname extends Route['pathname']> {
   pathname: Pathname;
   children: React.ReactNode;
   query?: PageProps<Pathname>['query'];
-  apiData?: PageProps<Pathname>['apiData'];
 }
 
 initSentry();
 
 const PageNextJs = <Pathname extends Route['pathname']>(props: Props<Pathname>) => {
-  const { title, description, opengraph, canonical } = metadata.generate(props, props.apiData);
-
   useGetCsrfToken();
   useAdblockDetect();
 
@@ -32,22 +28,11 @@ const PageNextJs = <Pathname extends Route['pathname']>(props: Props<Pathname>) 
   return (
     <>
       <Head>
-        <title>{ title }</title>
-        <meta name="description" content={ description }/>
-        { canonical && <link rel="canonical" href={ canonical }/> }
-
-        { /* OG TAGS */ }
-        <meta property="og:title" content={ opengraph.title }/>
-        { opengraph.description && <meta property="og:description" content={ opengraph.description }/> }
-        <meta property="og:image" content={ opengraph.imageUrl }/>
-        <meta property="og:type" content="website"/>
-
-        { /* Twitter Meta Tags */ }
+        {/* Title and meta tags are removed since metadata.generate is no longer used */}
+        <title>Stellus Explorer</title>
+        <meta name="description" content="Default description"/>
         <meta name="twitter:card" content="summary_large_image"/>
         <meta property="twitter:domain" content={ config.app.host }/>
-        <meta name="twitter:title" content={ opengraph.title }/>
-        { opengraph.description && <meta name="twitter:description" content={ opengraph.description }/> }
-        <meta property="twitter:image" content={ opengraph.imageUrl }/>
       </Head>
       { props.children }
     </>
